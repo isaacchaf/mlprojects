700bce7ab955de5298b0647df260ce2b93cfe8d4
isaacchaf <iechavez1998@gmail.com>
2025-01-24 11:51:31 -0600

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_datail: sys):
     _, _, exc_tb = error_datail.exc_info()
@@ -15,5 +15,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-
